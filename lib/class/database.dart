59d072701cda572f9d2d2dbefc9349a1ab055cd4
Dan Lévy <danlevy.ca@icloud.com>
2022-02-17 23:10:52 -0500

    Changement de la version du sdk Dart.

--- lib/class/database.dart
+++ lib/class/database.dart
@@ -1,9 +1,12 @@
-// ignore: avoid_web_libraries_in_flutter
-// import 'dart:js';
-
 import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'user.dart';
 
-class MongoDatabase {}
+class MongoDatabase {
+  
+  void getUser() {
+    var response = await http.get(Uri.parse('http://192.168.50.100:3000/'));
+
+  }
+}
