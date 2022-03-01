7cfb5e33da1860001470c36623cc9aeba99ec9b6
OlivierPoitras <poitrasolivier@gmail.com>
2022-03-01 18:00:06 -0500

    Amélioration visuelle des routes faisant partie du Dashboard.

--- front-end/src/app/home/components/history/history.component.ts
+++ front-end/src/app/home/components/history/history.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
+  
+  transactions = [{id:1, name:'Achat de 3.5 parts de Tesla Inc.'}, {id:2, name:'Vente de 0.5 parts de Google.'},
+  {id:3, name:'Achat de 3.5 parts de Tesla Inc.'}, {id:4, name:'Achat de 0.5 parts de Google.'},
+  {id:1, name:'Achat de 3.5 parts de Tesla Inc.'}, {id:2, name:'Vente de 4.5 parts de Nvidia.'},
+  {id:1, name:'Vente de 3.5 parts de Tesla Inc.'}, {id:2, name:'Achat de 0.5 parts de Google.'}];
 
   constructor() { }
 
