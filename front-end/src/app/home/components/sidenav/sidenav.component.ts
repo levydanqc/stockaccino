70916941f9125fd219f13983c53098e4cff43bd4
Dan Lévy <danlevy.ca@icloud.com>
2022-03-01 19:51:26 -0500

    Ajout de l'affichage du nom de la route.

--- front-end/src/app/home/components/sidenav/sidenav.component.ts
+++ front-end/src/app/home/components/sidenav/sidenav.component.ts
@@ -4,16 +4,26 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-
+  title: string = 'Stockaccino';
   isExpanded: boolean = false;
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    // get the current url
+    this.router.events.subscribe((url: any) => {
+      if (url.url === '/') {
+        this.title = 'Tableau de bord';
+      } else if (url.url === '/history') {
+        this.title = 'Historique des transactions';
+      } else if (url.url === '/transactions') {
+        this.title = 'Transactions';
+      } else if (url.url === '/settings') {
+        this.title = 'Paramètres';
+      }
+    });
   }
 
-
+  ngOnInit(): void {}
 }
