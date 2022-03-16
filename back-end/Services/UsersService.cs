ab4242312dfb92d906a4e31ef2df0dd3ed3393b2
OlivierPoitras <poitrasolivier@gmail.com>
2022-03-16 11:27:06 -0400

    Modification de UsersService

--- back-end/Services/UsersService.cs
+++ back-end/Services/UsersService.cs
@@ -2,37 +2,97 @@ using Stockaccino.Models;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
-namespace Stockaccino.Services;
-
-public class UsersService
+namespace Stockaccino.Services
 {
-    private readonly IMongoCollection<User> _usersCollection;
-
-    public UsersService(
-        IOptions<UserDatabaseSettings> userDatabaseSettings)
+    public interface IUserService
     {
-        var mongoClient = new MongoClient(
-            userDatabaseSettings.Value.ConnectionString);
-
-        var mongoDatabase = mongoClient.GetDatabase(
-            userDatabaseSettings.Value.DatabaseName);
-
-        _usersCollection = mongoDatabase.GetCollection<User>(
-            userDatabaseSettings.Value.UsersCollectionName);
+        bool IsAnExistingUser(string userName);
+        bool IsValidUserCredentials(string userName, string password);
+        string GetUserRole(string userName);
     }
 
-    public async Task<List<User>> GetAsync() =>
-        await _usersCollection.Find(_ => true).ToListAsync();
+    public class UsersService : IUserService
+    {
+        private readonly IMongoCollection<User> _usersCollection;
 
-    public async Task<User?> GetAsync(string email) =>
-        await _usersCollection.Find(x => x.Email == email).FirstOrDefaultAsync();
+        // Pour tester les users:
+        private readonly IDictionary<string, string> _users = new Dictionary<string, string>
+        {
+            { "test1", "password1" },
+            { "test2", "password2" },
+            { "admin", "securePassword" }
+        };
 
-    public async Task CreateAsync(User newUser) =>
-        await _usersCollection.InsertOneAsync(newUser);
+        // inject your database here for user validation
+        public UsersService(
+            IOptions<UserDatabaseSettings> userDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                userDatabaseSettings.Value.ConnectionString);
 
-    public async Task UpdateAsync(string id, User updatedUser) =>
-        await _usersCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
+            var mongoDatabase = mongoClient.GetDatabase(
+                userDatabaseSettings.Value.DatabaseName);
 
-    public async Task RemoveAsync(string id) =>
-        await _usersCollection.DeleteOneAsync(x => x.Id == id);
+            _usersCollection = mongoDatabase.GetCollection<User>(
+                userDatabaseSettings.Value.UsersCollectionName);
+
+        }
+
+        public async Task<List<User>> GetAsync() =>
+            await _usersCollection.Find(_ => true).ToListAsync();
+
+        public async Task<User?> GetAsync(string email) =>
+            await _usersCollection.Find(x => x.Email == email).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(User newUser) =>
+            await _usersCollection.InsertOneAsync(newUser);
+
+        public async Task UpdateAsync(string id, User updatedUser) =>
+            await _usersCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
+
+        public async Task RemoveAsync(string id) =>
+            await _usersCollection.DeleteOneAsync(x => x.Id == id);
+
+
+        public bool IsValidUserCredentials(string userName, string password)
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            return _users.TryGetValue(userName, out var p) && p == password;
+        }
+
+        public bool IsAnExistingUser(string userName)
+        {
+            return _users.ContainsKey(userName);
+        }
+
+        public string GetUserRole(string userName)
+        {
+            if (!IsAnExistingUser(userName))
+            {
+                return string.Empty;
+            }
+
+            if (userName == "admin")
+            {
+                return UserRoles.Admin;
+            }
+
+            return UserRoles.BasicUser;
+        }
+
+    }
+    public static class UserRoles
+    {
+        public const string Admin = nameof(Admin);
+        public const string BasicUser = nameof(BasicUser);
+    }
 }
