fa392241bb5439aa89f8a3ad6da0063b0c0c8dac
OlivierPoitras <poitrasolivier@gmail.com>
2022-03-16 11:04:24 -0400

    Installation du NuGet JwtBearer de Microsoft.

--- back-end/Infrastructure/JwtTokenConfig.cs
+++ back-end/Infrastructure/JwtTokenConfig.cs
@@ -1,18 +1,22 @@
-using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
 
-// Dans les projets de type SDK comme celui-là, plusieurs attributs d'assembly définis
-// historiquement dans ce fichier sont maintenant automatiquement ajoutés pendant
-// la génération et renseignés avec des valeurs définies dans les propriétés du projet.
-// Pour plus d'informations sur les attributs à inclure et sur la personnalisation
-// de ce processus, consultez : https://aka.ms/assembly-info-properties
+namespace JwtAuthDemo.Infrastructure
+{
+    public class JwtTokenConfig
+    {
+        [JsonPropertyName("secret")]
+        public string Secret { get; set; }
 
+        [JsonPropertyName("issuer")]
+        public string Issuer { get; set; }
 
-// La définition de ComVisible sur False rend les types dans cet assembly invisibles
-// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir
-// de COM, définissez l'attribut ComVisible sur True pour ce type.
+        [JsonPropertyName("audience")]
+        public string Audience { get; set; }
 
-[assembly: ComVisible(false)]
+        [JsonPropertyName("accessTokenExpiration")]
+        public int AccessTokenExpiration { get; set; }
 
-// Le GUID suivant concerne l'ID de typelib si ce projet est exposé à COM.
-
-[assembly: Guid("23af7cca-ddd7-42b1-8249-4c6661413455")]
+        [JsonPropertyName("refreshTokenExpiration")]
+        public int RefreshTokenExpiration { get; set; }
+    }
+}
