4692c5728306f87c6d262736cd4adf4e1310728e
Dan Levy <danlevy.ca@icloud.com>
2022-03-19 12:47:38 -0400

    refractor api call for users.

--- front-end/src/app/home/components/dashboard/dashboard.component.ts
+++ front-end/src/app/home/components/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-
+import { UserService } from 'src/app/service/user.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   message!: string;
 
-  constructor(private cookieService: CookieService) {}
+  constructor(
+    private cookieService: CookieService,
+    private _userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    // DELETE: only for debug
+    this._userService.getUsers().subscribe((data) => console.log(data));
+  }
 
   deconnexion() {
     //TODO: https://github.com/levydanqc/stockaccino/issues/7
