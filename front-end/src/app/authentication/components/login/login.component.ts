b21c5a3467f2de32712c55370eec2f2a62364a0d
levydanqc <58832530+levydanqc@users.noreply.github.com>
2022-03-19 12:15:32 -0400

    Fix redirection and method for login.

--- front-end/src/app/authentication/components/login/login.component.ts
+++ front-end/src/app/authentication/components/login/login.component.ts
@@ -5,11 +5,10 @@ import { UserService } from 'src/app/service/user.service';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   @Input() getEmailError!: () => string;
@@ -18,47 +17,36 @@ export class LoginComponent implements OnInit {
   public form!: FormGroup;
   email!: FormControl;
   password!: FormControl;
-  private users : IUser[] = [];
-  //user!: IUser;
+  user!: IUser;
   hide: boolean = true;
   invalid: boolean = false;
 
-  constructor(private controlContainer: ControlContainer, private _userService: UserService,
-    private router: Router, private cookieService: CookieService) {}
+  constructor(
+    private controlContainer: ControlContainer,
+    private _userService: UserService,
+    private router: Router,
+    private cookieService: CookieService
+  ) {}
 
   ngOnInit(): void {
     this.form = this.controlContainer.control as FormGroup;
     this.form.addControl('pwd', this.pwd);
     this.email = this.form.get('email') as FormControl;
     this.password = this.form.get('pwd') as FormControl;
-    this._userService.getUsers().subscribe(data => this.users = data)
-    //this._userService.verifyUser(this.email.value, this.password.value).subscribe(data => this.user = data)
   }
 
   onSubmit() {
-    var estValide = this.verifyUser(this.email.value, this.password.value);
-    console.log("EstValide: ", estValide);
-    if (estValide) {
+    let user = this._userService
+      .verifyUser(this.email.value, this.password.value)
+      .subscribe((data) => (this.user = data));
+    if (user) {
+      // TODO: https://github.com/levydanqc/stockaccino/issues/7
       this.router.navigate(['/']);
       this.cookieService.delete('UserEmail');
-      this.cookieService.set('UserEmail', this.email.value)
-    }
-    else {
-      console.log("Connection refusée.");
+      this.cookieService.set('UserEmail', this.email.value);
+    } else {
+      console.log('Connection refusée.');
       this.invalid = true;
     }
   }
-
-  // Retourne true si l'utilisateur s'est bien connecté, false si le email ou mot de passe est incorrect.
-  verifyUser(email: string, password: string) {
-    var estValide = false;
-    this.users.forEach(user => {
-      if (user.Email == email && user.Password == password)
-        estValide = true;
-    });
-    if (estValide)
-      return true;
-    return false;
-  }
-  
 }
--- front-end/src/app/home/components/dashboard/dashboard.component.ts
+++ front-end/src/app/home/components/dashboard/dashboard.component.ts
@@ -1,25 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
   message!: string;
 
-  constructor(private cookieService: CookieService) { }
-
-  ngOnInit(): void {
-    if (this.cookieService.get('UserEmail')) {
-      this.message = "Bienvenue, " + this.cookieService.get('UserEmail');
-    }
-  }
+  constructor(private cookieService: CookieService) {}
 
   deconnexion() {
     this.cookieService.delete('UserEmail');
     location.reload();
   }
-
 }
