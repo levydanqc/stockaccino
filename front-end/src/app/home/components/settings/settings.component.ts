574c95bf0e1f1d06c48b0aee09f7c49f819d11cf
Dan Levy <danlevy.ca@icloud.com>
2022-03-19 12:12:35 -0400

    fix redirection and login.

--- front-end/src/app/home/components/settings/settings.component.ts
+++ front-end/src/app/home/components/settings/settings.component.ts
@@ -4,19 +4,17 @@ import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
   email!: string;
   prenom!: string;
   nom!: string;
 
-  constructor(private cookieService: CookieService) { }
+  constructor(private cookieService: CookieService) {}
 
   ngOnInit(): void {
-    if (this.cookieService.get('UserEmail')) {
-      this.email = this.cookieService.get('UserEmail');
-    }
+    // TODO: get user data from database
+    this.email = this.cookieService.get('UserEmail');
   }
-
 }
--- front-end/src/app/service/auth.service.ts
+++ front-end/src/app/service/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private cookieService: CookieService) {}
 
   isLoggedIn() {
-    return true;
+    // TODO: https://github.com/levydanqc/stockaccino/issues/7
+    return this.cookieService.check('UserEmail');
   }
 }
