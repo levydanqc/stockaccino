f85890d4818606b35c3c1a91238b2e21de9fd5c2
Dan Levy <danlevy.ca@icloud.com>
2022-03-21 20:58:23 -0400

    refractor settings

--- front-end/src/app/home/components/settings/settings.component.ts
+++ front-end/src/app/home/components/settings/settings.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { ControlContainer, FormControl, FormGroup } from '@angular/forms';
+import {
+  ControlContainer,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { UserService } from 'src/app/service/user.service';
+
+export type Mode = 'show' | 'modify';
 
 @Component({
   selector: 'app-settings',
@@ -9,83 +16,78 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
-  email!: string;
-  prenom!: string;
-  nom!: string;
-  currentMode!: string;
-  errorEmail?: string;
-  public form!: FormGroup;
-  emailForm!: FormControl;
+  currentMode: string = 'show';
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    nom: new FormControl('', [Validators.required]),
+    prenom: new FormControl('', [Validators.required]),
+  });
 
   constructor(
     private cookieService: CookieService,
-    private _userService: UserService,
-    private controlContainer: ControlContainer,
-    ) {}
+    private _userService: UserService
+  ) {}
 
   ngOnInit(): void {
-    this._userService.getUserById(this.cookieService.get("id")).subscribe(data => {
-      this.email = data.Email;
-      this.prenom = data.Prenom;
-      this.nom = data.Nom;
-    });
-    this.currentMode = "show";
-    this.form = this.controlContainer.control as FormGroup;
-    this.form.addControl('emailForm', this.emailForm);
-    this.emailForm = this.form.get('emailForm') as FormControl;
+    this._userService
+      .getUserById(this.cookieService.get('id'))
+      .subscribe((data) => {
+        this.form.get('email')?.setValue(data.Email);
+        this.form.get('prenom')?.setValue(data.Prenom);
+        this.form.get('nom')?.setValue(data.Nom);
+      });
+  }
+
+  isShow() {
+    return this.currentMode == 'show';
+  }
+
+  placeholder(input: string) {
+    if (input == 'email') {
+      return this.isShow() ? '' : this.form.get('email');
+    } else if (input == 'prenom') {
+      return this.isShow() ? '' : this.form.get('prenom');
+    } else if (input == 'nom') {
+      return this.isShow() ? '' : this.form.get('nom');
+    }
+    return '';
+  }
+
+  value(input: string) {
+    if (input == 'email') {
+      return this.isShow() ? this.form.get('email') : '';
+    } else if (input == 'prenom') {
+      return this.isShow() ? this.form.get('prenom') : '';
+    } else if (input == 'nom') {
+      return this.isShow() ? this.form.get('nom') : '';
+    }
+    return '';
   }
 
   submitForm() {
-    let email!: string;
-    let prenom!: string;
-    let nom!: string;
-
-    console.log("Value du formControl email: ", this.emailForm.value);
-
-    if ((<HTMLInputElement>document.getElementById("email")).value != "") {
-      if (!this.validerEmail((<HTMLInputElement>document.getElementById("email")).value)) {
-        this.errorEmail = "L'adresse courriel est invalide.";
-      }
-      else {
-        email = (<HTMLInputElement>document.getElementById("email")).value;
-        this.errorEmail = undefined;
-      }
-    }
-    if ((<HTMLInputElement>document.getElementById("prenom")).value != "") {
-      prenom = (<HTMLInputElement>document.getElementById("prenom")).value;
-    }
-    if ((<HTMLInputElement>document.getElementById("nom")).value != "") {
-      nom = (<HTMLInputElement>document.getElementById("nom")).value;
-    }
-
-    if (this.errorEmail == undefined && email) {
-      this._userService.updateUser(this.cookieService.get("id"), email, nom, prenom);
-      setTimeout(()=>{
+    if (this.form.valid) {
+      this._userService.updateUser(
+        this.cookieService.get('id'),
+        this.form.get('email')?.value,
+        this.form.get('nom')?.value,
+        this.form.get('prenom')?.value
+      );
+      setTimeout(() => {
         window.location.reload();
       }, 100);
-      this.switchToShow();
-    }
-    else if (this.errorEmail == undefined){
-      this._userService.updateUser(this.cookieService.get("id"), undefined, nom, prenom);
-      setTimeout(()=>{
-        window.location.reload();
-      }, 100);
-      this.switchToShow();
+      this.switch('show');
     }
   }
 
-  switchToModify() {
-    this.currentMode = "modify";
-  }
+  getEmailError = (): string => {
+    const email = this.form.get('email') as FormControl;
+    if (email.hasError('required')) {
+      return 'Vous devez entrer une adresse email';
+    }
+    return email.hasError('email') ? "L'adresse email n'est pas valide" : '';
+  };
 
-  switchToShow() {
-    this.currentMode = "show";
-  }
-
-  validerEmail(email: string): boolean {
-    let regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-    if (regexp.test(email))
-      return true;
-    else return false;
+  switch(mode: Mode) {
+    this.currentMode = mode;
   }
 }
