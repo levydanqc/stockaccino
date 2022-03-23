320f63e05c67f9f66b9e3d014753ae331e3c640d
Dan Levy <danlevy.ca@icloud.com>
2022-03-22 23:15:28 -0400

    Ajout du model de carte

---
+++ back-end/Models/Carte.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Stockaccino.Models;
+
+public class Carte
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("number")]
+    public string Number { get; set; } = null!;
+
+    [BsonElement("month")]
+    public string Month { get; set; } = null!;
+
+    [BsonElement("year")]
+    public string Year { get; set; } = null!;
+
+    [BsonElement("cvv")]
+    public string CVV { get; set; } = null!;
+}
