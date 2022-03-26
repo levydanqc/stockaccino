ece6d4384da2676aa3c961a646e9cf2bbbec6cea
Dan Levy <danlevy.ca@icloud.com>
2022-03-26 13:36:11 -0400

    Cleaning API Models

--- back-end/Models/Carte.cs
+++ back-end/Models/Carte.cs
@@ -9,15 +9,11 @@ public class Carte
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
-    [BsonElement("number")]
     public string Number { get; set; } = null!;
 
-    [BsonElement("month")]
     public string Month { get; set; } = null!;
 
-    [BsonElement("year")]
     public string Year { get; set; } = null!;
 
-    [BsonElement("cvv")]
     public string CVV { get; set; } = null!;
 }
--- back-end/Models/Transaction.cs
+++ back-end/Models/Transaction.cs
@@ -3,26 +3,21 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Stockaccino.Models;
 
-public class User
+public class Transaction
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
-    [BsonElement("timestamp")]
     [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
     public DateTime Timestamp { get; set; }
 
-    [BsonElement("quantite")]
     public double Quantite { get; set; }
 
-    [BsonElement("ask")]
     public double Ask { get; set; }
 
-    [BsonElement("bid")]
     public double Bid { get; set; }
 
-    [BsonElement("symbol")]
     public string Symbol { get; set; } = null!;
 
     public Boolean IsBuying { get; set; }
