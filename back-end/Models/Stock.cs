fc89320bd62766a1118e7332b74bf369d23def62
Dan Levy <danlevy.ca@icloud.com>
2022-03-29 18:49:20 -0400

    Ajout du model d'un stock

---
+++ back-end/Models/Stock.cs
@@ -0,0 +1,19 @@
+﻿namespace Stockaccino.Models;
+
+public class Stock
+{
+    double high;
+    double low;
+    double close;
+    double open;
+    double volume;
+
+    public Stock(double high, double low, double close, double open, double volume)
+    {
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.open = open;
+        this.volume = volume;
+    }
+}
