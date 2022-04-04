b3c911645da5a756e91062860b0205e24fedf6ca
OlivierPoitras <poitrasolivier@gmail.com>
2022-04-04 11:28:12 -0400

    Fix boutons

--- front-end/src/app/home/components/chart/chart.component.ts
+++ front-end/src/app/home/components/chart/chart.component.ts
@@ -64,7 +64,7 @@ export class ChartComponent implements OnInit, OnDestroy, OnChanges {
     this.getStockData();
     setTimeout(() => {
       this.isLoading = false;
-    }, 1000)
+    }, 2000)
   }
 
   ngOnDestroy() {
