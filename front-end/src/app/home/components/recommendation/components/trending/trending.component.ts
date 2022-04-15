0fbdc04a3e626be6c27831585958baa64873c67f
Dan Levy <danlevy.ca@icloud.com>
2022-04-15 12:29:37 -0400

    cleanup trending

--- front-end/src/app/home/components/recommendation/components/trending/trending.component.ts
+++ front-end/src/app/home/components/recommendation/components/trending/trending.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
-import { User } from 'src/app/classes/user';
 import { Trending } from 'src/app/classes/yahoo/trending';
-import { UserService } from 'src/app/services/user.service';
 import { YahooService } from 'src/app/services/yahoo.service';
-import { screeners, Screener } from 'src/app/classes/yahoo/suggestion';
 @Component({
   selector: 'app-trending',
   templateUrl: './trending.component.html',
@@ -12,8 +9,7 @@ import { screeners, Screener } from 'src/app/classes/yahoo/suggestion';
 })
 export class TrendingComponent implements OnInit {
   trending: Trending[] = [];
-  suggestions: Screener[] = [];
-  loading: boolean = false;
+  loading: boolean = true;
   // Fake data only for enabling loop of carousel
   _images: any[] = [
     { path: '', width: 0, height: 0 },
@@ -25,7 +21,6 @@ export class TrendingComponent implements OnInit {
 
   constructor(
     private yahooService: YahooService,
-    // private userService: UserService,
     private _cookieService: CookieService
   ) {}
 
@@ -35,11 +30,5 @@ export class TrendingComponent implements OnInit {
       this.trending = data;
       this.loading = false;
     });
-
-    // screeners.forEach((screener) => {
-    //   this.yahooService.getSuggestion(screener).subscribe((data: Screener) => {
-    //     this.suggestions.push(data);
-    //   });
-    // });
   }
 }
