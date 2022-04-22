df032a51e71cc81c5427d9337702342cee9364a5
OlivierPoitras <poitrasolivier@gmail.com>
2022-04-22 14:10:30 -0400

    Corrections dashboard

--- front-end/src/assets/constants.ts
+++ front-end/src/assets/constants.ts
@@ -1,6 +1,6 @@
 export abstract class Constants {
-  static readonly LOCAL_URL: string = 'https://localhost:7056/';
-  static readonly BASE_URL: string = 'https://api.danlevy.ca/stockaccino/';
+  static readonly BASE_URL: string = 'https://localhost:7056/';
+  //static readonly BASE_URL: string = 'https://api.danlevy.ca/stockaccino/';
   static readonly USER_URL: string = this.BASE_URL + 'users/';
   static readonly YAHOO_URL: string = this.BASE_URL + 'yahoo/';
   static readonly AUTOCOMPLETE_URL: string = this.YAHOO_URL + 'autocomplete/';
