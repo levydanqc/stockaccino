239b2c49c531d9a2ec6b5f515ea20e4edcb48f8b
Dan Levy <danlevy.ca@icloud.com>
2022-05-17 14:19:25 -0400

    remote url

--- front-end/src/assets/constants.ts
+++ front-end/src/assets/constants.ts
@@ -1,7 +1,7 @@
 export abstract class Constants {
-  static readonly BASE_URL: string = 'https://localhost:7056/';
+  static readonly LOCAL_URL: string = 'https://localhost:7056/';
   static readonly DOMAIN: string = 'https://api.danlevy.ca/';
-  // static readonly BASE_URL: string = this.DOMAIN + 'stockaccino/';
+  static readonly BASE_URL: string = this.DOMAIN + 'stockaccino/';
   static readonly USER_URL: string = this.BASE_URL + 'users/';
   static readonly YAHOO_URL: string = this.BASE_URL + 'yahoo/';
   static readonly AUTOCOMPLETE_URL: string = this.YAHOO_URL + 'autocomplete/';
